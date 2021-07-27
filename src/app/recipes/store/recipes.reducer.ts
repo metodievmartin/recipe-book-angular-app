e3a0331c14ab25735fb37f4330ae2b8f1951e53b
Martin Metodiev <martin.metodiew@gmail.com>
2021-07-27 22:57:30 +0300

    Add recipe actions,reducer & effects

---
+++ src/app/recipes/store/recipes.reducer.ts
@@ -0,0 +1,25 @@
+import { Recipe } from '../recipe.model';
+import * as RecipesActions from './recipes.actions';
+
+export interface State {
+    recipes: Recipe[]
+}
+
+const initialState: State = {
+    recipes: []
+}
+
+export function recipeReducer(
+    state = initialState,
+    action: RecipesActions.Types
+) {
+    switch (action.type) {
+        case RecipesActions.SET_RECIPES:
+            return {
+                ...state,
+                recipes: [...action.payload]
+            }
+        default:
+            return state;
+    }
+}
